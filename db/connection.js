db94a0fd8d02f380d14c5d0b0b741cb8c4d3d1a2
Ricardo Vitor <ricardovdev@hotmail.com>
2024-10-25 19:54:28 -0300

    Hiding database password through a dotenv archive and concluding project modularization

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: "postgresql://mascots_clinica_user:8AiZmO9PdEzzrQBW79Q1jfAfRLR2BWK1@dpg-csdukk2j1k6c73fpriv0-a.oregon-postgres.render.com/mascots_clinica",
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
